02304c3b4c869317ee9ba79ebfcef49beb0362fc
JoaoRobin <245146660+JoaoRobin@users.noreply.github.com>
2025-12-29 04:48:12 +0000

    Uso do input
    Foi feito o uso do input para escrever o nome

--- primeiro.py
+++ primeiro.py
@@ -9,8 +9,10 @@ print ("Olá Mundo")
 print ("BOM DIA TSI")
 
 v=100
-
 print (v) #Mostra o valor da váriavel v
 
 v= "Agora sou uma string"
 print (v)
+
+nome=input ("Introduza o seu nome: ")
+print ("Olá", nome)
